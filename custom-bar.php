5250297a6b244e7fedcf95a9028c32315bb9ff7c
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-13 23:38:27 +0100

    Create custom-bar.php

---
+++ custom-bar.php
@@ -0,0 +1,45 @@
+<?php 
+
+$sold_Moyenne_Semaine;
+echo('
+
+    <div class="custom-bar-chart">
+        <ul class="y-axis">
+            <li><span>10.000</span></li>
+            <li><span>8.000</span></li>
+            <li><span>6.000</span></li>
+            <li><span>4.000</span></li>
+            <li><span>2.000</span></li>
+            <li><span>0</span></li>
+        </ul>
+        <div class="bar">
+            <div class="title">Lundi</div>
+            <div class="value tooltips" data-original-title="8.500" data-toggle="tooltip" data-placement="top">85%</div>
+        </div>
+        <div class="bar ">
+            <div class="title">Mardi</div>
+            <div class="value tooltips" data-original-title="5.000" data-toggle="tooltip" data-placement="top">50%</div>
+        </div>
+        <div class="bar ">
+            <div class="title">Mercredi</div>
+            <div class="value tooltips" data-original-title="6.000" data-toggle="tooltip" data-placement="top">60%</div>
+        </div>
+        <div class="bar ">
+            <div class="title">Jeudi</div>
+            <div class="value tooltips" data-original-title="4.500" data-toggle="tooltip" data-placement="top">45%</div>
+        </div>
+        <div class="bar">
+            <div class="title">Vendredi</div>
+            <div class="value tooltips" data-original-title="3.200" data-toggle="tooltip" data-placement="top">32%</div>
+        </div>
+        <div class="bar ">
+            <div class="title">Samedi</div>
+            <div class="value tooltips" data-original-title="6.200" data-toggle="tooltip" data-placement="top">62%</div>
+        </div>
+        <div class="bar">
+            <div class="title">Dimanche</div>
+            <div class="value tooltips" data-original-title="7.500" data-toggle="tooltip" data-placement="top">75%</div>
+        </div>
+    </div>
+');
+?>
