dd2d95e04ec2412c0c9330a46e38385e2e1e18f3
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-13 23:47:30 +0100

    Update connexion_bdd.php

--- BDD/connexion_bdd.php
+++ BDD/connexion_bdd.php
@@ -5,14 +5,11 @@
  $dbname = 'mmp-interface';
  $username = 'root';
  $password = '';
- $port = 3308;
  
-
  try //test de conection a la bdd
  {
      $pdo = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
      echo'<h1>Link with bdd Good!</h1>';
-
  }
  catch (PDOException $e)  //exception en cas de problème de conection a la bdd
  {
