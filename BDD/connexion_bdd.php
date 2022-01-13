b391b968a02a9e206d52a4da5f990cdab7186859
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-13 21:18:26 +0100

    Create connexion_bdd.php

---
+++ BDD/connexion_bdd.php
@@ -0,0 +1,19 @@
+<?php
+
+ //Variable de link avec la bdd
+ $host = 'localhost';
+ $dbname = 'mmp-interface';
+ $username = 'root';
+ $password = '';
+
+ try //test de conection a la bdd
+ {
+     $pdo = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
+     echo'<h1>Link with bdd Good!</h1>';
+
+ }
+ catch (PDOException $e)  //exception en cas de problème de conection a la bdd
+ {
+     die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage()); 
+ } 
+?>
