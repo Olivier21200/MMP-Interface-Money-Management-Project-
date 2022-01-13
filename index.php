6f1dfbe66666ba1fd18b4ec5a78a5a6a0423675b
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-13 20:36:37 +0100

    main update

--- index.php
+++ index.php
@@ -178,7 +178,7 @@
         <!-- sidebar menu start-->
         <ul class="sidebar-menu" id="nav-accordion">
           <p class="centered"><a href="profile.html"><img src="img/ui-sam.jpg" class="img-circle" width="80"></a></p>
-          <h5 class="centered">Sam Soffes</h5>
+          <h5 class="centered">Olivier Sirugue</h5>
           <li class="mt">
             <a class="active" href="index.html">
               <i class="fa fa-dashboard"></i>
@@ -279,7 +279,7 @@
           <div class="col-lg-9 main-chart">
             <!--CUSTOM CHART START -->
             <div class="border-head">
-              <h3>USER VISITS</h3>
+              <h3 class='text-green'>Compt n°1</h3>
             </div>
             <div class="custom-bar-chart">
               <ul class="y-axis">
@@ -291,34 +291,79 @@
                 <li><span>0</span></li>
               </ul>
               <div class="bar">
-                <div class="title">JAN</div>
+                <div class="title">Lundi</div>
                 <div class="value tooltips" data-original-title="8.500" data-toggle="tooltip" data-placement="top">85%</div>
               </div>
               <div class="bar ">
-                <div class="title">FEB</div>
+                <div class="title">Mardi</div>
                 <div class="value tooltips" data-original-title="5.000" data-toggle="tooltip" data-placement="top">50%</div>
               </div>
               <div class="bar ">
-                <div class="title">MAR</div>
+                <div class="title">Mercredi</div>
                 <div class="value tooltips" data-original-title="6.000" data-toggle="tooltip" data-placement="top">60%</div>
               </div>
               <div class="bar ">
-                <div class="title">APR</div>
+                <div class="title">Jeudi</div>
                 <div class="value tooltips" data-original-title="4.500" data-toggle="tooltip" data-placement="top">45%</div>
               </div>
               <div class="bar">
-                <div class="title">MAY</div>
+                <div class="title">Vendredi</div>
                 <div class="value tooltips" data-original-title="3.200" data-toggle="tooltip" data-placement="top">32%</div>
               </div>
               <div class="bar ">
-                <div class="title">JUN</div>
+                <div class="title">Samedi</div>
                 <div class="value tooltips" data-original-title="6.200" data-toggle="tooltip" data-placement="top">62%</div>
               </div>
               <div class="bar">
-                <div class="title">JUL</div>
+                <div class="title">Dimanche</div>
                 <div class="value tooltips" data-original-title="7.500" data-toggle="tooltip" data-placement="top">75%</div>
               </div>
             </div>
+
+            <div class="border-head">
+              <h3 class='text-green'>Compt n°2</h3>
+            </div>
+
+            <div class="custom-bar-chart">
+              <ul class="y-axis">
+                <li><span>10.000</span></li>
+                <li><span>8.000</span></li>
+                <li><span>6.000</span></li>
+                <li><span>4.000</span></li>
+                <li><span>2.000</span></li>
+                <li><span>0</span></li>
+              </ul>
+              <div class="bar">
+                <div class="title">Lundi</div>
+                <div class="value tooltips" data-original-title="8.500" data-toggle="tooltip" data-placement="top">85%</div>
+              </div>
+              <div class="bar ">
+                <div class="title">Mardi</div>
+                <div class="value tooltips" data-original-title="5.000" data-toggle="tooltip" data-placement="top">50%</div>
+              </div>
+              <div class="bar ">
+                <div class="title">Mercredi</div>
+                <div class="value tooltips" data-original-title="6.000" data-toggle="tooltip" data-placement="top">60%</div>
+              </div>
+              <div class="bar">
+                <div class="title">Jeudi</div>
+                <div class="value tooltips" data-original-title="4.500" data-toggle="tooltip" data-placement="top">45%</div>
+              </div>
+              <div class="bar">
+                <div class="title">Vendredi</div>
+                <div class="value tooltips" data-original-title="3.200" data-toggle="tooltip" data-placement="top">32%</div>
+              </div>
+              <div class="bar ">
+                <div class="title">Samedi</div>
+                <div class="value tooltips" data-original-title="6.200" data-toggle="tooltip" data-placement="top">62%</div>
+              </div>
+              <div class="bar">
+                <div class="title">Dimanche</div>
+                <div class="value tooltips" data-original-title="7.500" data-toggle="tooltip" data-placement="top">75%</div>
+              </div>
+             
+            </div>
+            
             <!--custom chart end-->
             <div class="row mt">
               <!-- SERVER STATUS PANELS -->
