32c4e6e56e2d674142536786afff572fb0a25601
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-14 20:55:39 +0100

    Update main.php

--- class/main.php
+++ class/main.php
@@ -1,11 +1,13 @@
 <?php 
     require_once('client.php');
+    require_once('compts.php');
 
      $client1 = new Client();
-
      $client1->__toString();
-     
      var_dump($client1);
+
+     $compt1 = new Compts();
+     var_dump($compt1);
 
    
 ?>
