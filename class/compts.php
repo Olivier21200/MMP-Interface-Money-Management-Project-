5389a66e45d180d0ffb96f52c93a0b6acfc96117
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-14 20:40:26 +0100

    Update compts.php

--- class/compts.php
+++ class/compts.php
@@ -3,7 +3,79 @@
 //Class qui représente un compts
 class compts 
 { 
+    private $_id;
+    private $_proprietaire;
+    private $_typeCompt;
+    private $_plafond;
+    private $_interet;
+    private $_solde;
+    private $_dateOuverture;
+    private $_dateFermeture;
 
+    //Constructeur de la class compt
+    public function __construct($id=null, $proprietaire=null, $typeCompt=null, $plafond=null, $interet=null, $solde=null, $dateOuverture=null, $dateFermeture=null)
+    {
+        $this->_id = $id;
+        $this->_proprietaire = $proprietaire;
+        $this->_typeCompt = $typeCompt;
+        $this->_plafond = $plafond;
+        $this->_interet = $interet;
+        $this->_solde = $solde;
+        $this->_dateOuverture = $dateOuverture;
+        $this->_dateFermeture = $dateFermeture;
+    }
+
+    //                          //
+    //  SETTER DES ATTRIBUTS    // 
+    //                          //
+
+    //Setter de l'id du compts
+    public function set_id($id) 
+    {
+        $this->_id = $id;
+    }
+
+    //Setter de l'id du compts
+     public function set_id($id) 
+     {
+         $this->_id = $id;
+     }
+
+    //Setter de l'id du compts
+    public function set_id($id) 
+    {
+        $this->_id = $id;
+    }
+
+    //Setter de l'id du compts
+     public function set_id($id) 
+     {
+         $this->_id = $id;
+     }
+
+    //Setter de l'id du compts
+    public function set_id($id) 
+    {
+        $this->_id = $id;
+    }
+
+    //Setter de l'id du compts
+     public function set_id($id) 
+     {
+         $this->_id = $id;
+     }
+
+    //Setter de l'id du compts
+    public function set_id($id) 
+    {
+        $this->_id = $id;
+    }
+
+    //Setter de l'id du compts
+     public function set_id($id) 
+     {
+         $this->_id = $id;
+     }
 }
 
 ?>
