21a9996af3c4e4c823e585757fb5a0d04083f2ed
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-14 23:37:17 +0100

    Update main.php

--- class/main.php
+++ class/main.php
@@ -5,13 +5,28 @@
     
     include_once('../bdd/connexion_bdd.php');
 
+    $clientManager = new clientManager($pdo);
 
-     $client1 = new Client();
+    echo $clientManager->get(1);
+
+
+     $client1 = new Client(9,'Emma','girard',20,'F',0640052760,'8c rue du chateau','Beaune',21200,'emma123@gmail.com','etudiante',1);
      $client1->__toString();
-     var_dump($client1);
+     $clientManager->add($client1);
+
+    // $clientManager->remove(15);
+     //$clientManager->remove(14);
+     //$clientManager->remove(13);
+     //var_dump( $clientManager->getList());
+
+
+
+     //var_dump($client1);
 
      $compt1 = new Compts();
-     var_dump($compt1);
+     //var_dump($compt1);
+     
+     
 
    
 ?>
