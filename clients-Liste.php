9ca71aa358ee6c2dac6dc6fe55c7b2b11f2bb6f8
Olivier21200 <olivier21.gonzalez@gmail.com>
2022-01-16 23:44:49 +0100

    Update clients-Liste.php

--- clients-Liste.php
+++ clients-Liste.php
@@ -279,7 +279,7 @@
               <h3 class='text-green'>Compt n°1</h3>
             </div>
 
-        <h3><i class="fa fas fa-address-book"></i> Liste des compts client</h3>
+        <h3 class="text-green"><i class="fa fas fa-address-book"></i > Liste des compts client :</h3>
         
           <!-- page start-->
           <div class="content-panel">
@@ -317,6 +317,8 @@
                     <td class="center hidden-phone">X</td>
                     <td class="center hidden-phone">X</td>
                   </tr>
+                  
+                  
                  </tbody>
               </table>
             </div>
@@ -327,12 +329,7 @@
    
           </div>
 
-          <!-- /col-lg-9 END SECTION MIDDLE -->
-          <!-- **********************************************************************************************************************************************************
-              RIGHT SIDEBAR CONTENT
-              *********************************************************************************************************************************************************** -->
           
-              
           <!-- /col-lg-3 -->
         </div>
         <!-- /row -->
@@ -349,7 +346,7 @@
          
           Created by Olivier Sirugue 
         </div>
-        <a href="index.html#" class="go-top">
+        <a href="index.php" class="go-top">
           <i class="fa fa-angle-up"></i>
           </a>
       </div>
@@ -364,6 +361,9 @@
   <script src="lib/jquery.scrollTo.min.js"></script>
   <script src="lib/jquery.nicescroll.js" type="text/javascript"></script>
   <script src="lib/jquery.sparkline.js"></script>
+  <script type="text/javascript" language="javascript" src="lib/advanced-datatable/js/jquery.js"></script>
+  <script type="text/javascript" language="javascript" src="lib/advanced-datatable/js/jquery.dataTables.js"></script>
+  <script type="text/javascript" src="lib/advanced-datatable/js/DT_bootstrap.js"></script>
   <!--common script for all pages-->
   <script src="lib/common-scripts.js"></script>
   <script type="text/javascript" src="lib/gritter/js/jquery.gritter.js"></script>
@@ -434,7 +434,67 @@
     }
   </script>
    <!--script for this page-->
-  
+   <script type="text/javascript">
+    /* Formating function for row details */
+    function fnFormatDetails(oTable, nTr) {
+      var aData = oTable.fnGetData(nTr);
+      var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+      sOut += '<tr><td>Rendering engine:</td><td>' + aData[1] + ' ' + aData[4] + '</td></tr>';
+      sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
+      sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+      sOut += '</table>';
+
+      return sOut;
+    }
+
+    $(document).ready(function() {
+      /*
+       * Insert a 'details' column to the table
+       */
+      var nCloneTh = document.createElement('th');
+      var nCloneTd = document.createElement('td');
+      nCloneTd.innerHTML = '<img src="lib/advanced-datatable/images/details_open.png">';
+      nCloneTd.className = "center";
+
+      $('#hidden-table-info thead tr').each(function() {
+        this.insertBefore(nCloneTh, this.childNodes[0]);
+      });
+
+      $('#hidden-table-info tbody tr').each(function() {
+        this.insertBefore(nCloneTd.cloneNode(true), this.childNodes[0]);
+      });
+
+      /*
+       * Initialse DataTables, with no sorting on the 'details' column
+       */
+      var oTable = $('#hidden-table-info').dataTable({
+        "aoColumnDefs": [{
+          "bSortable": false,
+          "aTargets": [0]
+        }],
+        "aaSorting": [
+          [1, 'asc']
+        ]
+      });
+
+      /* Add event listener for opening and closing details
+       * Note that the indicator for showing which row is open is not controlled by DataTables,
+       * rather it is done here
+       */
+      $('#hidden-table-info tbody td img').live('click', function() {
+        var nTr = $(this).parents('tr')[0];
+        if (oTable.fnIsOpen(nTr)) {
+          /* This row is already open - close it */
+          this.src = "lib/advanced-datatable/media/images/details_open.png";
+          oTable.fnClose(nTr);
+        } else {
+          /* Open this row */
+          this.src = "lib/advanced-datatable/images/details_close.png";
+          oTable.fnOpen(nTr, fnFormatDetails(oTable, nTr), 'details');
+        }
+      });
+    });
+  </script>
 </body>
 
 </html>
